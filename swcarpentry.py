c6968489bbca31ff8e6b23e2f0d5c23f1af24772
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-01-16 22:55:20 -0800

    got basic setup for sql query

--- swcarpentry.py
+++ swcarpentry.py
@@ -1,4 +1,14 @@
+import functools
 from pyspark.sql import SparkSession
+
+
+def explore_people(peopleDF):
+    """Explore people data."""
+    # temp view to explore data
+    peopleDF.createOrReplaceTempView("people")
+    # use .sql() to explore dataframe using SQL programmatically
+    sqlDF = spark.sql("SELECT * FROM people")
+    sqlDF.show()
 
 
 if __name__ == "__main__":
@@ -9,7 +19,8 @@ if __name__ == "__main__":
             .config("spark.some.config.option", "some-value") \
             .getOrCreate()
 
-    # csv file will be transformed into dataframe
+    # csv file transformed into dataframe
     peopleDF = spark.read.load("data/swcarpentry/person.csv", format="csv")
+surveyDF = spark.read.load("data/swcarpentry/survey.csv", format="csv")
 
     spark.stop()
