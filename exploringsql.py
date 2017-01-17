40b94f920e318117ce7aa43f498a77338de3a434
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-01-16 18:44:19 -0800

    taking advantage of provided data and examples

--- exploringsql.py
+++ exploringsql.py
@@ -1,4 +1,18 @@
-from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
 
-conf = SparkConf().setMaster("local").setAppName("My App")
-sc = SparkContext(conf=conf)
+# entry point into all functionality in Spark is the SparkSession class
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+# creates a DataFrame based on the content of a JSON file
+df = spark.read.json("testweet.json")
+# displays content of dataframe to stout
+df.show()
+# print schema in tree format
+df.printSchema()
+
+# select only the name column
+df.select("name").show()
